67948d8273f82cf672373d1d646cf0d701ade6d4
Professor1345 <alfulaani1345@gmail.com>
2024-02-07 04:31:23 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="bg-[rgb(45,44,41)] min-h-screen font-poppins">
+      <Header />
+      <hr />
+    </div>
+  );
+};
+
+export default App;
